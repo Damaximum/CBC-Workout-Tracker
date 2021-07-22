11f067109a5196adb8910ebb209e1a8877cd685f
Damaximum <79727388+Damaximum@users.noreply.github.com>
2021-07-21 21:57:08 -0700

    fixed a bug with totalDuration calculations

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,13 @@ app.get('/stats', (req, res) => {
 // ----------------------------------------------------------------------------------------------------
 
 app.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' }
+            }
+        }
+    ])
         .sort({ day: -1 })
         .then(workoutDB => {
             // console.log(workoutDB);
